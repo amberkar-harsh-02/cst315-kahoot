2a7a41eede13e3f5e20b7d3759d801929fc79b94
Harsh Manohar Amberkar <hamberkar@csumb.edu>
2026-09-08 14:32:07 -0700

    Update gitignore to exclude SQLite files and initialize database on startup

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, WebSocket
+from database import engine
+import models
+
+# This command tells SQLAlchemy to create all tables defined in models.py
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CST 315 Kahoot Clone")
 
 @app.get("/")
 async def root():
-    return {"message": "Server is running!"}
+    return {"message": "Database and Server are running!"}
 
 @app.websocket("/ws/test")
 async def websocket_test(websocket: WebSocket):
